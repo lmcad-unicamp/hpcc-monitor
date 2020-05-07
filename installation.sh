d58b80be58c0cec15474ec6dc9dbc49e1e5cce0a
William Felipe <theorangewill@gmail.com>
2020-05-06 22:58:09 -0300

    Update Installation Script

--- installation.sh
+++ installation.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
 sudo apt install zabbix-agent python3 python3-pip -y
-sudo apt-get install python3-dev libmysqlclient-dev
+sudo apt-get install python3-dev libmysqlclient-dev -y
 sudo -H pip3 install pyzabbix boto3 apache-libcloud mysqlclient
