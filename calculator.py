12e356f74af60fd2b79b5e9ee357b04e41cb4690
William Felipe <theorangewill@gmail.com>
2020-05-11 20:48:00 -0300

    Adding Wastage Calculator scripts and API

---
+++ calculator.py
@@ -0,0 +1,149 @@
+import logging
+import os
+import zapi as monitorserver
+from wastageapi import HistoryWastage
+from datetime import datetime, timezone
+from sendemail import quotaexceeded_email
+
+HEURISTIC = 'heuristic-1'
+
+heuristics_keys = {'heuristic-1': {'accelerated':     'wastage.gpu.util',
+                                   'compute':         'wastage.cpu.util',
+                                   'generalpurpose':  'system.cpu.util'
+                                                      + '[all,idle,avg1]',
+                                   'memory':          'wastage.memory.util',
+                                   'storage':         'wastage.cpu.util'
+                                   }
+                   }
+
+home = os.path.dirname(os.path.realpath(__file__))
+
+logger = logging.getLogger(str(__file__))
+logger.setLevel(logging.INFO)
+fh = logging.FileHandler(home+"/log/calculator.log")
+ch = logging.StreamHandler()
+formatter = logging.Formatter('[%(asctime)s] - [%(levelname)5s] - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+HISTORY_ITEMS_FILE = home+"/files/history.items"
+NOW = int(datetime.timestamp(datetime.utcnow().replace(tzinfo=timezone.utc)))
+
+# Get hosts from Monitor Server
+hostsFromMonitorServer = monitorserver.get_hosts(
+                                        output=['name'],
+                                        filter={'status': '0'},
+                                        macros=['macro', 'value'],
+                                        templates=['templateid', 'name'],
+                                        groups=['groupsid', 'name'],
+                                        items=['itemid', 'key_',
+                                               'delay', 'value_type'],
+                                        user=True,
+                                        family=True
+                                        )
+
+history = HistoryWastage(HISTORY_ITEMS_FILE)
+
+# If the host does not exist anymore, it is deleted
+for host in history.get_hosts():
+    if not [h for h in hostsFromMonitorServer if h == host]:
+        history.delete_host(host)
+
+# If the host does not have history, it is added
+# Update the prices
+for host in hostsFromMonitorServer:
+    if host not in history.get_hosts():
+        history.add_host(host)
+        price_timestamp = 0
+    else:
+        price_timestamp = history.get_price_timestamp(host)
+    prices = monitorserver.get_history(host=hostsFromMonitorServer[host],
+                                       itemkey='cloud.price',
+                                       since=price_timestamp,
+                                       till=NOW)
+    history.set_pricing_history(host, prices)
+
+
+# For each host, calculate wastage
+for host in hostsFromMonitorServer:
+    history.set_heuristic(host, HEURISTIC)
+
+    # In this heuristic, each family of instances has a specific item to look
+    if HEURISTIC == 'heuristic-1':
+        # Get the name of the item
+        item = heuristics_keys['heuristic-1'][hostsFromMonitorServer[host][
+                                                                    'family']]
+        # Get the history of this item
+        item_history = history.get_item_history(host, HEURISTIC, item)
+        # If the item does not have a history
+        if not item_history:
+            item_history = {'sum': 0.0, 'timestamp': 0}
+        # Get the values of the item since the last value requested till now
+        values = monitorserver.get_history(
+                                        host=hostsFromMonitorServer[host],
+                                        itemkey=item,
+                                        since=item_history['timestamp'],
+                                        till=NOW)
+        # If there is new values
+        if values:
+            # Convert the sample time to hour (which is the unit for prices)
+            item_delay = hostsFromMonitorServer[host]['items'][item]['delay']
+            value_delay = monitorserver.convert_to_hour(item_delay)
+
+            # Calculate the wastage of each sample of value
+            item_wastage_calculated = 0.0
+            for v in values:
+                # Convert the type of the value(the function returns as string)
+                value = monitorserver.convert_value_type(
+                    v['value'],
+                    hostsFromMonitorServer[host]['items'][item]['value_type'])
+                # Find the price of the sample based on price history
+                value_price = history.find_price(host, v['timestamp'])
+                # Calculate the wastage
+                # In this heuristic, takes % out of each sample and multiply
+                # with the price of the sample time (in hours)
+                wastage = value / 100.0 * value_price * value_delay
+                # Add wastage to the user
+                history.add_user_wastage(hostsFromMonitorServer[host]['user'],
+                                         wastage, v['timestamp'])
+                # This variable stores the wastage calculated in this execution
+                item_wastage_calculated = item_wastage_calculated + wastage
+
+            # Store the total wastage of this item and the last timestamp
+            item_history['sum'] = item_history['sum'] + item_wastage_calculated
+            item_history['timestamp'] = values[-1]['timestamp']
+            history.set_item_history(host, HEURISTIC, item, item_history)
+
+            # Get the wastage of the host and sum with the wastage calculated
+            host_wastage_calculated = history.get_host_wastage(host)
+            host_wastage_calculated = (host_wastage_calculated
+                                       + item_wastage_calculated)
+            history.set_host_wastage(host, host_wastage_calculated)
+
+            # Send to montior server
+            monitorserver.send_item(hostsFromMonitorServer[host], 'wastage',
+                                    host_wastage_calculated)
+
+
+# Check if quota of the user has been exceeded
+for user in history.get_users():
+    if (history.get_user_attribute(user, 'permonth')
+       >= history.get_user_attribute(user, 'quota')):
+        try:
+            emails = monitorserver.get_admins_email()
+            emails.append(monitorserver.get_user_email(hostsFromMonitorServer[
+                                                       host]['user']))
+            quotaexceeded_email(emails,
+                                history.get_user_attribute(user, 'quota'),
+                                history.get_user_attribute(user, 'permonth'))
+        except monitorserver.NotFoudException as e:
+            logger.error("[CALCULATOR] Quota exceeded " + user
+                         + ". Could not send email to admins and user "
+                         + user + ": "
+                         + str(e))
+        else:
+            logger.info("[CALCULATOR] Quota exceeded. Sending email to admins "
+                        + "and user "
+                        + history.get_user_attribute(user, 'permonth'))
---
+++ wastageapi.py
@@ -0,0 +1,157 @@
+import logging
+import os
+import inspect
+import json
+import MySQLdb
+from datetime import datetime, timezone
+from pprint import pprint
+
+
+home = os.path.dirname(os.path.realpath(__file__))
+logger = logging.getLogger(str(inspect.getouterframes(inspect.currentframe()
+                                                      )[-1].filename))
+DBSERVER = (open(home+"/private/db_server", "r")).read().strip('\n')
+DBUSER = (open(home+"/private/db_user", "r")).read().strip('\n')
+DBPASSWORD = (open(home+"/private/db_password", "r")).read().strip('\n')
+
+# Get acces to database
+con = MySQLdb.connect(DBSERVER, DBUSER, DBPASSWORD)
+con.select_db('db_quota_manager')
+cursor = con.cursor()
+
+CURRENT_MONTH = int(datetime.utcnow().replace(tzinfo=timezone.utc).month)
+
+
+class HistoryWastage:
+    def __init__(self, HISTORY_ITEMS_FILE):
+        self.HISTORY_ITEMS_FILE = HISTORY_ITEMS_FILE
+        self.history_wastage = {}
+        # Get history of wastage from file
+        if os.path.isfile(HISTORY_ITEMS_FILE):
+            try:
+                self.history_wastage = json.loads((open(HISTORY_ITEMS_FILE,
+                                                        'r')).read())
+            except json.decoder.JSONDecodeError:
+                pass
+
+        # Get user wastage and quota from database
+        self.users = {}
+        cursor.execute("SELECT * FROM User_Wastage")
+        for user in list(cursor.fetchall()):
+            self.users[user[0]] = {}
+            self.users[user[0]]['quota'] = float(user[1])
+            self.users[user[0]]['total'] = float(user[2])
+            self.users[user[0]]['permonth'] = float(user[3])
+            if user[4] == CURRENT_MONTH:
+                self.users[user[0]]['month'] = user[4]
+            else:
+                self.users[user[0]]['month'] = 0
+
+    def __del__(self):
+        # Update file
+        (open(self.HISTORY_ITEMS_FILE,
+              "w")).write(json.dumps(self.history_wastage))
+
+        # Update database
+        for user in self.users:
+            cursor.execute("UPDATE User_Wastage SET WastageTotal="
+                           + str(self.users[user]['total'])
+                           + ",WastageLastMonth="
+                           + str(self.users[user]['permonth'])
+                           + ",Month="+str(self.users[user]['month'])
+                           + " WHERE UserName=\'" + user
+                           + "\'")
+            con.commit()
+
+    # Get history of wastage
+    def get_history(self):
+        return self.history_wastage
+
+    # Get hosts of history
+    def get_hosts(self):
+        return [host for host in self.history_wastage]
+
+    # Get users
+    def get_users(self):
+        return self.users
+
+    # Add wastage to the user, per month and total
+    def add_user_wastage(self, user, wastage, wastage_timestamp):
+        current_wastage_month = self.users[user]['permonth']
+        current_wastage = self.users[user]['total']
+        if (int(datetime.fromtimestamp(wastage_timestamp).month)
+                == CURRENT_MONTH):
+            self.users[user]['permonth'] = current_wastage_month + wastage
+        self.users[user]['total'] = current_wastage + wastage
+
+    # Get an attribute from a user
+    def get_user_attribute(self, user, attribute):
+        return self.users[user][attribute]
+
+    # Delete host from history
+    def delete_host(self, host):
+        del self.history_wastage[host]
+        logger.info("[WASTAGEAPI] [delete_host] Host deleted " + host)
+
+    # Add new host to history
+    def add_host(self, host):
+        self.history_wastage[host] = {}
+        self.history_wastage[host]['wastage'] = 0
+        self.history_wastage[host]['prices'] = {}
+        self.history_wastage[host]['prices']['timestamps'] = []
+        self.history_wastage[host]['prices']['values'] = []
+        logger.info("[WASTAGEAPI] [add_host] Host added " + host)
+
+    # Get history of a host
+    def get_host_history(self, host):
+        return self.history_wastage[host]
+
+    # Set wastage of a host
+    def set_host_wastage(self, host, wastage):
+        self.history_wastage[host]['wastage'] = wastage
+
+    # Get wastage of a host
+    def get_host_wastage(self, host):
+        return self.history_wastage[host]['wastage']
+
+    # Set a heuristic for host if it exists:
+    def set_heuristic(self, host, heuristic):
+        if heuristic not in self.history_wastage[host]:
+            self.history_wastage[host][heuristic] = {}
+
+    # Get the last timestamp of price of host
+    def get_price_timestamp(self, host):
+        if self.history_wastage[host]['prices']['timestamps']:
+            return self.history_wastage[host]['prices']['timestamps'][-1]
+        else:
+            return 0
+
+    # Get price of the host in a specific timestamp
+    def find_price(self, host, timestamp):
+        prices = self.history_wastage[host]['prices']
+        for i in reversed(range(len(prices['timestamps']))):
+            if prices['timestamps'][i] <= timestamp:
+                return float(prices['values'][i])
+        return prices['values'][0]
+
+    # Set the history of price of host
+    def set_pricing_history(self, host, values):
+        new_timestamps = []
+        new_values = []
+        for v in values:
+            new_timestamps.append(int(v['timestamp']))
+            new_values.append(float(v['value']))
+        self.history_wastage[host]['prices']['timestamps'].extend(
+                                                                new_timestamps)
+        self.history_wastage[host]['prices']['values'].extend(new_values)
+
+    # Get history of an item
+    def get_item_history(self, host, HEURISTIC, item):
+        if item in self.history_wastage[host][HEURISTIC]:
+            return self.history_wastage[host][HEURISTIC][item]
+        else:
+            return {}
+
+    # Set history to an item
+    def set_item_history(self, host, HEURISTIC, item, history):
+        self.history_wastage[host][HEURISTIC][item] = history
