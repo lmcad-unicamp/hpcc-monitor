15052d545025fdfc48cdf0bcce7f27fd87efa0a5
William Felipe <theorangewill@gmail.com>
2021-04-03 21:58:45 -0400

    Experiment scripts

---
+++ experiments/unlimited-threads/experiments/script2.sh
@@ -0,0 +1,138 @@
+python3 script.py r52xlarge cg D 4 15
+python3 script.py i32xlarge cg D 4 15
+exit
+python3 script.py i34xlarge ft D 16
+python3 script.py r54xlarge ft D 16
+python3 script.py r58xlarge ft D 32
+python3 script.py r5a8xlarge ft D 32
+python3 script.py r5n8xlarge ft D 32
+python3 script.py r512xlarge ft D 32
+python3 script.py r5n12xlarge ft D 32
+
+#######################################################
+python3 script.py r5axlarge bt D 4 10
+python3 script.py r5xlarge bt D 4 10
+python3 script.py i32xlarge bt D 4
+python3 script.py r52xlarge bt D 4
+python3 script.py r5a2xlarge bt D 4
+python3 script.py t32xlarge bt D 4
+python3 script.py c54xlarge bt D 16
+python3 script.py c5a4xlarge bt D 16
+python3 script.py c5n4xlarge bt D 16
+python3 script.py i34xlarge bt D 16
+python3 script.py r54xlarge bt D 16
+python3 script.py c5a8xlarge bt D 25
+python3 script.py r58xlarge bt D 25
+python3 script.py r5a8xlarge bt D 25
+python3 script.py r5n8xlarge bt D 25
+python3 script.py c59xlarge bt D 36
+python3 script.py c5n9xlarge bt D 36
+python3 script.py c512xlarge bt D 36
+python3 script.py r512xlarge bt D 36
+python3 script.py r5n12xlarge bt D 36
+
+python3 script.py r5axlarge sp D 4
+python3 script.py r5xlarge sp D 4
+python3 script.py i32xlarge sp D 4
+python3 script.py r52xlarge sp D 4
+python3 script.py r5a2xlarge sp D 4
+python3 script.py t32xlarge sp D 4
+python3 script.py c54xlarge sp D 16
+python3 script.py c5a4xlarge sp D 16
+python3 script.py c5n4xlarge sp D 16
+python3 script.py i34xlarge sp D 16
+python3 script.py r54xlarge sp D 16
+python3 script.py c5a8xlarge sp D 25
+python3 script.py r58xlarge sp D 25
+python3 script.py r5a8xlarge sp D 25
+python3 script.py r5n8xlarge sp D 25
+python3 script.py c59xlarge sp D 36
+python3 script.py c5n9xlarge sp D 36
+python3 script.py c512xlarge sp D 36
+python3 script.py r512xlarge sp D 36
+python3 script.py r5n12xlarge sp D 36
+#######################################################
+python3 script.py r5axlarge cg D 4 15
+python3 script.py r5xlarge cg D 4 15
+python3 script.py i32xlarge cg D 8
+python3 script.py r52xlarge cg D 8
+python3 script.py r5a2xlarge cg D 8
+python3 script.py t32xlarge cg D 8
+python3 script.py c54xlarge cg D 16
+python3 script.py c5a4xlarge cg D 16
+python3 script.py c5n4xlarge cg D 16
+python3 script.py i34xlarge cg D 16
+python3 script.py r54xlarge cg D 16
+python3 script.py c5a8xlarge cg D 32
+python3 script.py r58xlarge cg D 32
+python3 script.py r5a8xlarge cg D 32
+python3 script.py r5n8xlarge cg D 32
+python3 script.py c59xlarge cg D 32
+python3 script.py c5n9xlarge cg D 32
+python3 script.py c512xlarge cg D 32
+python3 script.py r512xlarge cg D 32
+python3 script.py r5n12xlarge cg D 32
+
+
+python3 script.py r5axlarge ep E 4
+python3 script.py r5xlarge ep E 4
+python3 script.py i32xlarge ep E 8
+python3 script.py r52xlarge ep E 8
+python3 script.py r5a2xlarge ep E 8
+python3 script.py t32xlarge ep E 8
+python3 script.py c54xlarge ep E 16
+python3 script.py c5a4xlarge ep E 16
+python3 script.py c5n4xlarge ep E 16
+python3 script.py i34xlarge ep E 16
+python3 script.py r54xlarge ep E 16
+python3 script.py c5a8xlarge ep E 32
+python3 script.py r58xlarge ep E 32
+python3 script.py r5a8xlarge ep E 32
+python3 script.py r5n8xlarge ep E 32
+python3 script.py c59xlarge ep E 32
+python3 script.py c5n9xlarge ep E 32
+python3 script.py c512xlarge ep E 32
+python3 script.py r512xlarge ep E 32
+python3 script.py r5n12xlarge ep E 32
+
+python3 script.py r5axlarge is D 4
+python3 script.py r5xlarge is D 4
+python3 script.py i32xlarge is D 8
+python3 script.py r52xlarge is D 8
+python3 script.py r5a2xlarge is D 8
+python3 script.py t32xlarge is D 8
+python3 script.py c54xlarge is D 16
+python3 script.py c5a4xlarge is D 16
+python3 script.py c5n4xlarge is D 16
+python3 script.py i34xlarge is D 16
+python3 script.py r54xlarge is D 16
+python3 script.py c5a8xlarge is D 32
+python3 script.py r58xlarge is D 32
+python3 script.py r5a8xlarge is D 32
+python3 script.py r5n8xlarge is D 32
+python3 script.py c59xlarge is D 32
+python3 script.py c5n9xlarge is D 32
+python3 script.py c512xlarge is D 32
+python3 script.py r512xlarge is D 32
+python3 script.py r5n12xlarge is D 32
+
+python3 script.py r5axlarge lu D 4
+python3 script.py r5xlarge lu D 4
+python3 script.py i32xlarge lu D 8
+python3 script.py r52xlarge lu D 8
+python3 script.py r5a2xlarge lu D 8
+python3 script.py t32xlarge lu D 8
+python3 script.py c54xlarge lu D 16
+python3 script.py c5a4xlarge lu D 16
+python3 script.py c5n4xlarge lu D 16
+python3 script.py i34xlarge lu D 16
+python3 script.py r54xlarge lu D 16
+python3 script.py c5a8xlarge lu D 32
+python3 script.py r58xlarge lu D 32
+python3 script.py r5a8xlarge lu D 32
+python3 script.py r5n8xlarge lu D 32
+python3 script.py c59xlarge lu D 32
+python3 script.py c5n9xlarge lu D 32
+python3 script.py c512xlarge lu D 32
+python3 script.py r512xlarge lu D 32
+python3 script.py r5n12xlarge lu D 32
---
+++ experiments/unlimited-threads/selections/script2.sh
@@ -0,0 +1,27 @@
+#python3 script.py r5a8xlarge ft D 16
+#######################################################
+#python3 script.py r5a8xlarge bt D 16
+#python3 script.py c512xlarge bt D 25
+python3 script.py c59xlarge bt D 25
+
+#python3 script.py r5a8xlarge sp D 16
+python3 script.py c512xlarge sp D 25
+python3 script.py c59xlarge sp D 25
+#######################################################
+#python3 script.py r5a8xlarge cg D 16
+python3 script.py c512xlarge cg D 32
+python3 script.py c59xlarge cg D 32
+python3 script.py r5a2xlarge cg D 4 12
+python3 script.py t32xlarge cg D 4 12
+
+#python3 script.py r5a8xlarge ep E 16
+python3 script.py c512xlarge ep E 32
+python3 script.py c59xlarge ep E 32
+
+#python3 script.py r5a8xlarge is D 16
+python3 script.py c512xlarge is D 32
+python3 script.py c59xlarge is D 32
+
+#python3 script.py r5a8xlarge lu D 16
+python3 script.py c512xlarge lu D 32
+python3 script.py c59xlarge lu D 32
